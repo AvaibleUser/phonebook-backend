6f029225fa198c15ba86321ecdb8c9bfd86ef962
Dylan <100001088+AvaibleUser@users.noreply.github.com>
2023-01-04 06:01:30 +0000

    Add endpoint to fetch a single person

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -7,4 +7,15 @@ router.get("/persons", (req, res) => {
   res.json(persons);
 });
 
+router.get("/persons/:id", (req, res) => {
+  const id = +req.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
