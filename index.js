a3411a1f0825e9290e73ab3e92eb2756a7b517c3
Dylan <100001088+AvaibleUser@users.noreply.github.com>
2023-01-05 05:56:29 +0000

    Add logs with morgan package as middleware

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 const PORT = 3001;
@@ -6,7 +8,13 @@ const PORT = 3001;
 const personsRoutes = require("./src/routes/persons");
 const infoRoute = require("./src/routes/info");
 
+const morganFormat =
+  ":method :url :status :res[content-length] - :response-time ms :body";
+
+morgan.token("body", ({ body }) => (body?.name ? JSON.stringify(body) : " "));
+
 app.use(express.json());
+app.use(morgan(morganFormat));
 
 app.use("/api/persons", personsRoutes);
 app.use("/", infoRoute);
